5a081da092f2a7d09f80df96ff039d528f4067a6
BuildTools <unconfigured@null.spigotmc.org>
2015-10-30 12:03:50 +0900

    removed server.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var request = require('request');
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+
 //var ObjectId = require('mongodb').ObjectID;
 
-var dbUrl = 'mongodb://localhost:27017/btcusd';
+var dbUrl = 'mongodb://127.0.0.1:27017/btcusd';
 
 // bitfinex rest api get latest trade for USD
 
@@ -24,7 +24,7 @@ function get() {
    MongoClient.connect(dbUrl, function(err, db) {
 
 
-            request.get(options, function(error, response, data) {
+         request.get(options, function(error, response, data) {
 
                data = JSON.parse(data);
                console.log(data[0]['timestamp'] + ": " + data[0]['amount'] + " @ " + data[0]['price']);
@@ -35,13 +35,18 @@ function get() {
                   price: data[0]['price'],
                   amount: data[0]['amount'],
                   type: data[0]['type']
+               }, function(error, result) {
+                  if (error) console.log(error); //info about what went wrong
+                  if (result) console.log(result,  db.close()); //the _id of new object if successful
+                  process.exit();
+                 
                });
 
-               db.close();
-               return;
-            })
+          
+         })
 
    });
+
 };
 
 get();
